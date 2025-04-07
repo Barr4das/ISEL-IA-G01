f9ccb657a9b0d4be1643d2bb73995dbf18763e23
Barradas100Pai <gui1.barradas@gmail.com>
2025-04-07 11:22:57 +0100

    Update generateBoard.pl

--- trb1/generateBoard.pl
+++ trb1/generateBoard.pl
@@ -59,3 +59,41 @@ add_pieces(_, [_], [_]).
 add_pieces(PlayerSymbol, [HI, HI2 | TI], [HO, HI2 | Rest]) :-
     add_pieces_even(PlayerSymbol, [HI, HI2 | TI], [HO, HI2 | _]),
     add_pieces(PlayerSymbol, TI, Rest).
+
+fill_board([], _, _, []).
+
+fill_board([HI | TI], Player_rows, Idx, [HO | TO]) :-
+    Idx < Player_rows, !,
+    New_idx is Idx + 1,
+    process_row(HI, '\u25cb', Idx, HO),
+    fill_board(TI, Player_rows, New_idx, TO).
+
+fill_board([HI | TI], Player_rows, Idx, [HO | TO]) :-
+    Idx >= (Player_rows * 2) - 1, !,
+    New_idx is Idx + 1,
+    process_row(HI, '\u25cf', Idx, HO),
+    fill_board(TI, Player_rows, New_idx, TO).
+
+fill_board([HI | TI], Player_rows, Idx, [HI | TO]) :-
+    New_idx is Idx + 1,
+    fill_board(TI, Player_rows, New_idx, TO).
+
+process_row(Row, Piece, Idx, Result) :-
+    (   1 =:= Idx mod 2 
+    ->  reverse(Row, RevRow),
+        add_pieces(Piece, RevRow, RevResult),
+        reverse(RevResult, Result)
+    ;   add_pieces(Piece, Row, Result)
+    ).
+
+test:-
+    Board_size = 8,
+    generateBoard(Board, Board_size),
+    Player_rows is (Board_size-2) // 2,
+    fill_board(Board, Player_rows, 0, FinalBoard),
+    print_checkers(FinalBoard, Board_size).
+
+test_pieces :-
+    TestList = ['.','.','.','.','.','.','.'],
+    add_pieces("L", TestList, Ret),
+    write(Ret).
